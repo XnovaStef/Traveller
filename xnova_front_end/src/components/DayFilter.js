f956a3e8f4c62f1556f4cffa2d103a57bfdcd772
unknown <falletkamagate3@gmail.com>
2023-08-22 16:13:09 +0200

    Fallet update

--- xnova_front_end/src/components/DayFilter.js
+++ xnova_front_end/src/components/DayFilter.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native'; // Import ScrollView
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Import the FontAwesome icon
 import { BarChart } from 'react-native-chart-kit';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function DayFilter() {
-  // Example data for the last 24 hours
+  const [showDatePicker, setShowDatePicker] = useState(false); // Moved useState inside the component
+
   const paymentData = [
     100, 200, 150, 300, 250, 180, 280, 220, 350, 120,
     180, 260, 170, 220, 300, 240, 190, 280, 220, 150,
@@ -13,7 +16,7 @@ export default function DayFilter() {
   const hours = Array.from({ length: 24 }, (_, i) => i);
 
   const data = {
-    labels: hours.map(hour => hour.toString()), // Convert hours to strings
+    labels: hours.map(hour => hour.toString()),
     datasets: [
       {
         data: paymentData,
@@ -23,11 +26,16 @@ export default function DayFilter() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Statistiques paiements</Text>
-      <ScrollView horizontal> 
+      <View style={styles.header}>
+      <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+          <Icon name="calendar" size={30} color="#246EC3" style={styles.calendarIcon} />
+        </TouchableOpacity>
+        <Text style={styles.title}>Statistiques paiements</Text>
+      </View>
+      <ScrollView horizontal>
         <BarChart
           data={data}
-          width={hours.length * 50} // Adjust the width based on the number of hours
+          width={hours.length * 50}
           height={200}
           chartConfig={{
             backgroundColor: '#ffffff',
@@ -40,11 +48,16 @@ export default function DayFilter() {
         />
       </ScrollView>
 
-      <Text style={styles.title1}>Statistiques réservations</Text>
-      <ScrollView horizontal> 
+      <View style={styles.header}>
+      <TouchableOpacity onPress={() => setShowDatePicker(true)}>
+          <Icon name="calendar" size={30} color="#246EC3" style={styles.calendarIcon} />
+        </TouchableOpacity>
+        <Text style={styles.title1}>Statistiques réservations</Text>
+      </View>
+      <ScrollView horizontal>
         <BarChart
           data={data}
-          width={hours.length * 50} // Adjust the width based on the number of hours
+          width={hours.length * 50}
           height={200}
           chartConfig={{
             backgroundColor: '#ffffff',
@@ -56,6 +69,19 @@ export default function DayFilter() {
           style={styles.chart}
         />
       </ScrollView>
+      {showDatePicker && (
+        <DateTimePicker
+          value={new Date()} // Initial value for the picker
+          mode="date"
+          display="default"
+          onChange={(event, selectedDate) => {
+            if (selectedDate) {
+              // Handle selected date here
+            }
+            setShowDatePicker(false);
+          }}
+        />
+      )}
     </View>
   );
 }
@@ -70,20 +96,24 @@ const styles = StyleSheet.create({
   chart: {
     marginTop: 20,
   },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  calendarIcon: {
+    marginRight: 10,
+    top:10
+  },
   title: {
     color: '#246EC3',
     fontSize: 15,
     fontWeight: 'bold',
-    marginBottom: 10,
-    top:20,
-    right:25
+    top:10
   },
   title1: {
     color: '#246EC3',
     fontSize: 15,
     fontWeight: 'bold',
-    marginBottom: 0,
-    top:10,
-    right:25
+    top:10
   },
 });
